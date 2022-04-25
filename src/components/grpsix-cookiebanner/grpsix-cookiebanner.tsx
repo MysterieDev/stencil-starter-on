5b934986c9b1790c3063c0afc611e243fc37f322
Antje-Claudia <ant.schmollinger.21@lehre.mosbach.dhbw.de>
2022-04-25 15:31:05 +0200

    added grpsix-cookiebanner.css and grpsix-cookiebanner.tsx

---
+++ src/components/grpsix-cookiebanner/grpsix-cookiebanner.tsx
@@ -0,0 +1,18 @@
+import { Component, Host, h } from '@stencil/core';
+
+@Component({
+  tag: 'grpsix-cookiebanner',
+  styleUrl: 'grpsix-cookiebanner.css',
+  shadow: true,
+})
+export class GrpsixCookiebanner {
+
+  render() {
+    return (
+      <Host>
+        <slot></slot>
+      </Host>
+    );
+  }
+
+}
