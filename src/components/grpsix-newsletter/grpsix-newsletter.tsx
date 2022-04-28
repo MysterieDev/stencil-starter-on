d46c8f97224ed022742e22bc550220d2ae6a20df
olgaluisemerz <olga-merz@gmx.de>
2022-04-28 22:28:28 +0200

    Erstellung einer Funktion, die ein Danke ausgibt.

--- src/components/grpsix-newsletter/grpsix-newsletter.tsx
+++ src/components/grpsix-newsletter/grpsix-newsletter.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop, } from '@stencil/core';
+import { Component, Host, h, Prop, State } from '@stencil/core';
 
 @Component({
   tag: 'grpsix-newsletter',
@@ -9,45 +9,58 @@ import { Component, Host, h, Prop, } from '@stencil/core';
 export class GrpsixNewsletter {
 
   @Prop() websiteName: string;
-  
-  private buttonClick = () => {
-    alert(" - Danke, dass du dich für unseren Newsletter entschieden hast - ");
+
+  @State() message: string = "";
+  @State() value: string;
+  @State() selectValue: string;
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.value);
   }
-  
-  //let btn = document.getElementById("anmeldebtn");
-  //let message = document.getElementById("alert");
-  //btn.addEventListener("click", function(){
-  //message.style.visibility = "visible";
-  //});
+
+  handleChange(event) {
+    this.value = event.target.value;
+
+    if (event.target.validity.typeMismatch) {
+      console.log('this element is not valid')
+    }
+  }
+
+  private messageClick = () => {
+    this.message = "- Danke, dass du dich für unseren Newsletter entschieden hast -";
+    //alert ("- Danke, dass du dich für unseren Newsletter entschieden hast -");
+   }
 
   render() {
     return (
       <Host>
+        <slot>
         <div class="container-fluid">
         <div id="newsletter">
           <h2>{this.websiteName} Newsletter</h2>
           <p>Hier bekommst du jede Woche tolle Rezepte und praktische Tipps!</p>
-          <form>
+          <form onSubmit={(e) => this.handleSubmit(e)}>
             <div class="form-group">
               <input class="form-control" id="nameFeld" placeholder="Dein Name"></input>
             </div>
             <div class="form-group">
-              <input class="form-control" id="emailFeld" placeholder="Deine Email-Adresse"></input>
+              <input type="email" value={this.value} class="form-control" id="emailFeld" placeholder="Deine Email-Adresse"></input>
             </div>
             <div class="checkbox">
               <input type="checkbox" id="Datenschutz"></input>
               <label htmlFor="Datenschutz">  Ich habe die Datenschutzerklärung zur Kenntnis genommen.</label>
             </div>
 
-            <button class="form-control" id="anmeldebtn" onClick={this.buttonClick} >Jetzt Anmelden</button>
+            <button class="form-control" id="anmeldebtn" onClick={this.messageClick}>Jetzt Anmelden</button>
 
-            <div id="alert"> - Danke, dass du dich für unseren Newsletter entschieden hast - </div>
-            
+            <div>{this.message}</div>
+          
           </form>
         </div>
         </div>
 
-        <slot></slot>
+        </slot>
       </Host>
     );
   }
