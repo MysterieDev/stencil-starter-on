fa32a6ce9615ae63a70f63603657dbfadb9b354e
Julian Fleischmann <julian.fleischmann@mr-pixel.de>
2022-04-30 20:34:10 +0200

    recipe card funktioniert

--- src/components/grpsix-recipe-card/grpsix-recipe-card.tsx
+++ src/components/grpsix-recipe-card/grpsix-recipe-card.tsx
@@ -10,11 +10,6 @@ interface ApiData {
   imageUrl: string;
 }
 
-interface FakeAPI {
-  id: number,
-  name: string
-}
-
 @Component({
   tag: 'grpsix-recipe-card',
   styleUrl: 'grpsix-recipe-card.css',
@@ -23,91 +18,50 @@ interface FakeAPI {
 export class GrpsixRecipeCard {
 
   @State() apiData: ApiData[];
-  @State() myActualRecipe: any;
-  @State() myNewActualRecipe: any= "hal";
-  @State() myFakeApiData: FakeAPI[] = [
-    {id: 3, name: "Julian"},
-    {id: 4, name: "Fleischmann"}
-  ]
-
-
-  doSmth = () => {
-    // if(this.myActualRecipe === undefined) {return}
-
-    // console.log('sync 1');
-    // setTimeout(_ => console.log('sync 2'), 200);
-    // setTimeout(_ => this.myActualRecipe = this.apiData.find(element => element.id === 3000), 1000);
-    // setTimeout(_ => this.myNewActualRecipe = this.myNewActualRecipe.title, 2000);
-    // Promise.resolve().then(_ => console.log('sync 3'));
-    // Promise.resolve().then(_ => this.myActualRecipe = this.apiData.find(element => element.id === 3000));
-    // console.log('sync 4')
-    
-    
-    // window.onload = function () {
-    //   if (this.apiData === undefined) {
-    //       console.log("variable is nicht definiert!");
-    //       return;
-    //     } else {
-    //       console.log("Variable ist endlich definiert");
-    //       this.myActualRecipe = this.apiData.find(element => element.id === 3000);
-    //   }
-    // };
-   }
+  @State() myActualRecipe: any; // Any?
 
   componentWillLoad() {
-    fetch('./recipes.json')
+    return fetch('./recipes.json')
     .then(res => res.json())
     .then((apiRecipes: Array<ApiData>) => (this.apiData = apiRecipes))
-    // .then(this.doSmth)
+    .then(
+      recipe => {
+        return this.myActualRecipe = recipe.find(element => element.id === 2000);
+      }
+    )
     .catch(err => console.log(err));
-
-    window.onload = function () {
-      this.myActualRecipe = this.apiData.find(element => element.id === 3000);
-    }
-    
-    if (this.apiData === undefined) {
-      console.log("variable is nicht definiert!");
-      return;
-      } else {
-        console.log("Variable ist endlich definiert");
-        // this.myActualRecipe = this.apiData.find(element => element.id === 3000);
-    }
-
-    
-    
-    // // this.doSmth();
-
-    // window.onload(
-    //   if (this.apiData === undefined) {
-    //     console.log("ist undefiniert");
-    //   } else {
-    //     this.doSmth();
-    //   } 
-    // );
   }
+   
 
-
-  // componentWillLoad() {
+  // getRecipe () {
   //   return fetch('./recipes.json')
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       this.apiData = data;
-  //     });
+  //   .then(res => res.json())
+  //   .then((apiRecipes: Array<ApiData>) => (this.apiData = apiRecipes))
+  //   .then(
+  //     recipe => {
+  //       return this.myActualRecipe = recipe.find(element => element.id === 2000);
+  //     }
+  //   )
+  //   .catch(err => console.log(err));
   // }
 
-   
+  // Warum funktioniert das nicht????? --> recipe.find ist void????
+  // showRecipes () {
+  //   this.getRecipes().then(
+  //     recipe => {
+  //       this.myActualRecipe = recipe.find(element => element.id === 3000);
+        
+  //       console.log(this.myActualRecipe);
+  //     }
+  //   )
+  // }
+
 
   render() {
     return (
       <Host>
         <div>
-          {/* <h1>Das ist Rezept: {setTimeout((_ => this.myNewActualRecipe), 3000)}</h1> */}
-          <h1>Das ist Rezept: {this.myNewActualRecipe.title}</h1>
-          {/* {console.log("Api Data Render: " + this.apiData)} */}
-          {/* <button onClick={this.doSmth}>Rezept laden</button> */}
-          {/* {setTimeout(_ => console.log("myActualRecipe Render: " + this.myActualRecipe.title), 3000)} */}
-          {/* {console.log("myActualRecipe Render: " + this.myActualRecipe.title)} */}
-          {/* {console.log(this.myActualRecipe.title)} */}
+          <h1>Das ist Rezept: {this.myActualRecipe.title}</h1>
         </div>
       </Host>
     );
