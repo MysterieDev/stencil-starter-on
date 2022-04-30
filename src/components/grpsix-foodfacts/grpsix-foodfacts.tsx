deadfce1ecf7f0a7676c96ef24c868e4f2654513
olgaluisemerz <olga-merz@gmx.de>
2022-04-30 14:51:55 +0200

    Hinzufügen von Food Facts und Hover-Effekten

--- src/components/grpsix-foodfacts/grpsix-foodfacts.tsx
+++ src/components/grpsix-foodfacts/grpsix-foodfacts.tsx
@@ -62,9 +62,22 @@ export class GrpsixFoodfacts {
                   <p>
                     <br/>
                     <br/>
-                    blabla
+                    <u><b>1.</b> Erdnüsse sind eigentlich keine Nüsse.</u>
                   </p>
-                </details>
+                  <p> Tatsächlich ist der Name "Erdnuss" etwas irreführend, denn Erdnüsse werden botanisch gesehen nicht zu den Nüssen gezählt.
+                      Erdnüsse sind tatsächlich mehr mit Bohnen und Erbsen verwandt als mit Mandeln oder anderen Nüssen.
+                  </p>
+                  <p><u><b>2.</b> Super viele Nährstoffe enthalten.</u></p>
+                  <p> Erdnüsse schmecken nicht nur lecker, sondern enthalten viel pflanzliches Eiweiß, Fett und andere wichtige Nährstoffe, die sich positiv auf die Gesundheit auswirken können.
+                      Allerdings sind Erdnüsse ziemlich fetthaltig und somit kleine Kalorienbomben.</p>
+                  <p><u><b>3.</b> Kleine Protein-Wunder.</u></p>
+                  <p> Mit 25 Gramm Eiweiß stehen Erdnüsse sogar bei den proteinreichsten Nüssen und Samen ganz oben auf der Liste – sie werden nicht ohne Grund zu den Hülsenfrüchten gezählt.</p>
+                  <p><u><b>4.</b> Erdnüsse stärken Nerven und Gehirn.</u></p>
+                  <p> Da Erdnüsse einen hohen Gehalt an Magnesium haben, sind sie der perfekte Snack für geistige Anstrengungen, da das Nervensystem durch Magnesium vor Stress geschützt wird – die Nerven und die Muskeln profitieren davon.</p>
+                  <p><u><b>5.</b> Krankheiten kann vorgebeugt werden.</u></p>
+                  <p>Ob Herzinfarkt, Diabetes oder Schlaganfall: Erdnüsse können offenbar davor schützen und das Risiko deutlich senken. Das ergaben mehrere voneinander unabhängige Studien. Dabei genügt schon der Verzehr von kleinen Mengen (15 Gramm Erdnüsse täglich).</p>
+                  {/*Quelle der Erdnuss-facts: https://eatsmarter.de/ernaehrung/wie-gesund-ist/erdnuss*/}
+                </details> 
               </div>
             </div>
 
@@ -80,8 +93,23 @@ export class GrpsixFoodfacts {
                   <p>
                     <br/>
                     <br/>
-                    blabla
+                    <u><b>1.</b> "Junger Trieb".</u>
                   </p>
+                  <p> "Asparagus officinalis" nennen Botaniker den Gemüsespargel. Asparagus kommt aus dem Lateinischen bzw. dem Altgriechischen und heißt „junger Trieb“.
+                  </p>
+                  <p><u><b>2.</b> Grün, weiß oder doch lila?</u></p>
+                  <p> Der weiße Spargel wird auch Bleichspargel genannt, denn er bleibt nur so schön weiß, weil er ohne Sonnenlicht im Erdwall wächst.
+                    Lugt der Spargel aus der Erde, färbt er sich zunächst violett.
+                    Grüner Spargel wächst ohne Erdwall direkt im Sonnenlicht.</p>
+                  <p><u><b>3.</b> Grüner Spargel schmecht am würzigsten.</u></p>
+                  <p> Grüner Spargel bildet mit Hilfe des Sonnenlichts grünes Chlorophyll, aber auch mehr Carotin (eine Vorstufe von Vitamin A) und mehr Vitamin C.
+                    Zudem entwickeln sich mehr Aromastoffe, so dass grüner Spargel würziger schmeckt.</p>
+                  <p><u><b>4.</b> Nicht zu weit unten abschneiden!</u></p>
+                  <p> Wenn der Spargel bei der Ernte zu dicht am Wurzelstock abgeschnitten wird, schmeckt er später bitter.
+                    Denn die Teile nah an der Wurzel enthalten mehr Bitterstoffe.</p>
+                  <p><u><b>5.</b> Asparagusinsäure sorgt für Aroma.</u></p>
+                  <p>Verantwortlich für den typischen Spargel-Geschmack mit leicht bitterer Note ist vor allem die Asparagusinsäure. Sie entwickelt ihr Aroma jedoch erst beim Kochen.</p>
+                  {/*Quelle der Spargel-facts:https://aramark-foodboost.de/10-funfacts-spargel/*/}
                 </details>
               </div>
             </div>
