24d66a4bddd57bdc10dbf32d5bccdd17582f1bb1
Jessica <jes.guenther.21@lehre.mosbach.dhbw.de>
2022-05-02 18:32:46 +0200

    added introduction

--- src/components/pop-heading/readme.md
+++ src/components/pop-heading/readme.md
@@ -12,6 +12,19 @@
 | `heading` | `heading` |             | `string` | `undefined` |
 
 
+## Dependencies
+
+### Used by
+
+ - [pop-introduction](../pop-introduction)
+
+### Graph
+```mermaid
+graph TD;
+  pop-introduction --> pop-heading
+  style pop-heading fill:#f9f,stroke:#333,stroke-width:4px
+```
+
 ----------------------------------------------
 
 *Built with [StencilJS](https://stenciljs.com/)*
---
+++ src/components/pop-introduction/pop-introduction.tsx
@@ -0,0 +1,25 @@
+import { Component, Host, h, Prop, getAssetPath } from '@stencil/core';
+const popUpLogo = getAssetPath("/assets/pop-up-logo.png")
+
+@Component({
+  tag: 'pop-introduction',
+  styleUrl: 'pop-introduction.css',
+  shadow: true,
+})
+export class PopIntroduction {
+
+@Prop() introduction : any
+
+  render() {
+    return (
+      <Host>
+        <img class="popUpLogo" src={popUpLogo}/>
+        <pop-heading heading="PopUp - das ultimative Minispiel"></pop-heading>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+          sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+      </Host>
+    );
+  }
+
+}
