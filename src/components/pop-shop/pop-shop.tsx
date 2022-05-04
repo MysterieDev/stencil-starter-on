e5a59b98fdc3e13e323e04d0de52306f628a9c40
on21OE <oli.ebert.21@lehre.mosbach.dhbw.de>
2022-05-04 14:38:21 +0200

    added variable text content

--- src/components/pop-shop/pop-shop.tsx
+++ src/components/pop-shop/pop-shop.tsx
@@ -22,6 +22,9 @@ export class PopShop {
     const shopImage = this.element.shadowRoot.getElementById('mainImage');
     const prevShopImage = this.element.shadowRoot.getElementById('prevImage');
     const nextShopImage = this.element.shadowRoot.getElementById('nextImage');
+    const shopTitle = this.element.shadowRoot.getElementById('title');
+    const shopPrice = this.element.shadowRoot.getElementById('price');
+    const shopDescription = this.element.shadowRoot.getElementById('description');
     let currentImage = 3;
 
     function setPreviousImage() {
@@ -33,6 +36,9 @@ export class PopShop {
         prevShopImage.setAttribute('src', `${LogoShirt}`);
         shopImage.setAttribute('src', `${SweetShirt}`);
         nextShopImage.setAttribute('src', `${GangsterShirt}`);
+        shopTitle.innerText = "Popy Shirt";
+        shopPrice.innerText = "19,99 €";
+        shopDescription.innerText = "Damit wirst du bei deinem nächsten Kinobesuch zum Hit!";
       }
     }
 
@@ -45,6 +51,9 @@ export class PopShop {
         prevShopImage.setAttribute('src', `${SweetShirt}`);
         shopImage.setAttribute('src', `${GangsterShirt}`);
         nextShopImage.setAttribute('src', `${LogoSnapback}`);
+        shopTitle.innerText = "Gangster Shirt";
+        shopPrice.innerText = "19,99 €";
+        shopDescription.innerText = "Der ultimative Beweis für deine super kranke Coolness!";
       }
     }
 
@@ -54,31 +63,49 @@ export class PopShop {
           prevShopImage.setAttribute('src', `${SweetShirt}`);
           shopImage.setAttribute('src', `${GangsterShirt}`);
           nextShopImage.setAttribute('src', `${LogoSnapback}`);
+          shopTitle.innerText = "Gangster Shirt";
+          shopPrice.innerText = "19,99 €";
+          shopDescription.innerText = "Der ultimative Beweis für deine super kranke Coolness!";
           break;
         case 2:
           prevShopImage.setAttribute('src', `${GangsterShirt}`);
           shopImage.setAttribute('src', `${LogoSnapback}`);
           nextShopImage.setAttribute('src', `${PopUpShirt}`);
+          shopTitle.innerText = "PopUp Snapback";
+          shopPrice.innerText = "14,99 €";
+          shopDescription.innerText = "Werde der Teil Popy Crew und habe den Spaß deines Lebens!";
           break;
         case 3:
           prevShopImage.setAttribute('src', `${LogoSnapback}`);
           shopImage.setAttribute('src', `${PopUpShirt}`);
           nextShopImage.setAttribute('src', `${SweetSnapback}`);
+          shopTitle.innerText = "PopUp Shirt Big";
+          shopPrice.innerText = "19,99 €";
+          shopDescription.innerText = "Luftig, locker, leicht - Wie dein Lieblingspopcorn!";
           break;
         case 4:
           prevShopImage.setAttribute('src', `${PopUpShirt}`);
           shopImage.setAttribute('src', `${SweetSnapback}`);
           nextShopImage.setAttribute('src', `${LogoShirt}`);
+          shopTitle.innerText = "Sweet Snapback";
+          shopPrice.innerText = "14,99 €";
+          shopDescription.innerText = "Das ist super sweet, wirklich für jeden ein Need!";
           break;
         case 5:
           prevShopImage.setAttribute('src', `${SweetSnapback}`);
           shopImage.setAttribute('src', `${LogoShirt}`);
           nextShopImage.setAttribute('src', `${SweetShirt}`);
+          shopTitle.innerText = "PopUp Shirt Small";
+          shopPrice.innerText = "19,99 €";
+          shopDescription.innerText = "Klein, aber fein! Zeige deine Angehörigkeit zur Crew, ohne großes Tam-Tam!";
           break;
         case 6:
           prevShopImage.setAttribute('src', `${LogoShirt}`);
           shopImage.setAttribute('src', `${SweetShirt}`);
           nextShopImage.setAttribute('src', `${GangsterShirt}`);
+          shopTitle.innerText = "Popy Shirt";
+          shopPrice.innerText = "19,99 €";
+          shopDescription.innerText = "Damit wirst du bei deinem nächsten Kinobesuch zum Hit!";
           break;
       }
     }
@@ -99,7 +126,7 @@ export class PopShop {
             <div id="shopContent">
               <p id="title">PopUp Shirt</p>
               <p id="price">19,99 €</p>
-              <p id="description">Luftig, locker, leicht - wie dein Lieblingspopcorn</p>
+              <p id="description">Luftig, locker, leicht - Wie dein Lieblingspopcorn!</p>
               <pop-button text="In den Warenkorb" size="s" link="https://www.youtube.com/watch?v=dQw4w9WgXcQ" color="blue"></pop-button>
             </div>
           </div>
