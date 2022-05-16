6f8644894f14337e62a9eb93a8ad7249c2dbf2ce
Philipp Borutta <phb@koempf24.de>
2022-05-16 17:01:51 +0200

    added all cards & styled

--- src/components/card-flip/card-flip.tsx
+++ src/components/card-flip/card-flip.tsx
@@ -22,6 +22,34 @@ export class cardFlip{
               <slot name="back"></slot>
             </div>
             </div>
+
+            <div class="card">
+            <div class="front2">
+              <slot name="front2"></slot>
+            </div>
+            <div class="back2">
+              <slot name="back2"></slot>
+            </div>
+            </div>
+
+            <div class="card">
+          <div class="front3">
+              <slot name="front3"></slot>
+            </div>
+            <div class="back3">
+              <slot name="back3"></slot>
+            </div>
+            </div>
+
+            <div class="card">
+          <div class="front4">
+              <slot name="front4"></slot>
+            </div>
+            <div class="back4">
+              <slot name="back4"></slot>
+            </div>
+            </div>
+
             </div>
       </Host>
     );
